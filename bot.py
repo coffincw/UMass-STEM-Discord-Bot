0566c3ae37208134e6210b11c2727880e69e8dd1
Caleb Coffin <coffincw@gmail.com>
2019-03-04 18:35:52 -0500

    added basic commands and some memes

---
+++ bot.py
@@ -0,0 +1,37 @@
+import random
+from discord.ext.commands import Bot
+import asyncio
+
+BOT_PREFIX = "$"
+BOT_TOKEN = "NTUyMjU0NTk4Mjc5MDY5NzA4.D1823Q.77zzMatjVJRQ0OmyOENOheYS03w"
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+@client.command(name='8ball',
+                description="Answers from the 8ball",
+                pass_context = True)
+async def eight_ball(context):
+    possible_responses = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell'
+    ]
+    await client.say(random.choice(possible_responses))
+
+@client.command()
+async def square(number):
+    await client.say(str(number) + " squared is " + str(int(number) * int(number)))
+
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers: ")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(6)
+
+
+print("Test")
+
+client.loop.create_taks(list_servers())
+client.run(BOT_TOKEN)
