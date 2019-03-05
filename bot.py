6e707c0be24d2636017279331b39a83c1fba59d1
Caleb Coffin <coffincw@gmail.com>
2019-03-04 21:46:56 -0500

    adjusted draw()

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import random
+from io import BytesIO
+import discord
 from discord.ext.commands import Bot
 import asyncio
+from overlay import overlay_image
+from overlay import url_to_image
 
 BOT_PREFIX = "$"
 BOT_TOKEN = "NTUyMjU0NTk4Mjc5MDY5NzA4.D1823Q.77zzMatjVJRQ0OmyOENOheYS03w"
@@ -18,27 +22,43 @@ async def eight_ball(context):
     ]
     await client.say(random.choice(possible_responses))
 
-@client.command(name='draw')
-async def draw(message):
-    extension = ['.jpg', '.png', '.jpeg']
-    for ext in extension:
-        if message.content.endswith(ext):
-            
+# @client.event
+# async def on_message(message):
+#     print(message.attachments[0]['url'])
+
+@client.command(name='draw', pass_context = True)
+async def draw(ctx):
+    image_url = ''
+    # try:
+    #     image = message.attachments[0]['url']
+    # except IndexError:
+    #     extension = ['.jpg', '.png', '.jpeg']
+    #     for ext in extension:
+    #         if message.content.endswith(ext):
+    #             image = message.content
+    #     if (image == ''):
+    #         await client.say("Please input a valid image")
+    image_url = ctx.message.attachments[0]['url']
+    image = url_to_image(image_url)
+    image.show()
+    output = overlay_image(image, 'memes/marius/draw.png')
+    await client.send_file(ctx.message.channel, output)
+
 
 @client.command()
 async def square(number):
     await client.say(str(number) + " squared is " + str(int(number) * int(number)))
 
-async def list_servers():
-    await client.wait_until_ready()
-    while not client.is_closed:
-        print("Current servers: ")
-        for server in client.servers:
-            print(server.name)
-        await asyncio.sleep(6)
+# async def list_servers():
+#     await client.wait_until_ready()
+#     while not client.is_closed:
+#         print("Current servers: ")
+#         for server in client.servers:
+#             print(server.name)
+#         await asyncio.sleep(6)
 
 
 print("Test")
 
-client.loop.create_taks(list_servers())
+# client.loop.create_taks(list_servers())
 client.run(BOT_TOKEN)
--- overlay.py
+++ overlay.py
@@ -1,9 +1,18 @@
 from PIL import Image
+import requests
+from io import BytesIO
 
+def overlay_image(background, overlay_image):
+    overlay = Image.open(overlay_image) #opens the overlay image
+    
 
-def overlay(background_image, overlay_image):
-    background = Image.open(background_image)
-    overlay = Image.open(overlay_image)
-
-    background.paste(overlay, (0, 0), overlay)
+    background.paste(overlay, (0, 0), overlay) #pastes the overlay image on top of the background image
     return background
+
+def url_to_image(url):
+    print("test")
+    response = requests.get(url)
+    print("test1")
+    image = Image.open(BytesIO(response.content))
+    print("test2")
+    return image
