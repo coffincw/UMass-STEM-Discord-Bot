afdd04f88e9c07765bad2e5ffc7d0961846f0798
coffincw <coffincw@gmail.com>
2019-03-09 13:53:00 -0500

    fixed space under marius/barr in the draw command

--- overlay.py
+++ overlay.py
@@ -5,7 +5,7 @@ from io import BytesIO
 def overlay_image(target, overlay_image):
     overlay = Image.open(overlay_image)                                                     #opens the overlay image
     width, heigth = target.size                                                             # size of inputed image, used for scaling
-    white_background = Image.new("RGBA", (width + 500, heigth + 400), (255, 255, 255))      # white background created based on the size of the inputed image
+    white_background = Image.new("RGBA", (width + 500, heigth + 200), (255, 255, 255))      # white background created based on the size of the inputed image
     white_background.paste(target, (0, 0), target)                                          # paste the inputed image in the upper left hand corner
     white_background.paste(overlay, (width - 450, heigth - 450), overlay)                   # paste marius at the bottom right hand corner of the inputed image
     return white_background
