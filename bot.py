0fcee3abed6cbad7537cd3aa2e9d690ae54fec78
coffincw <coffincw@gmail.com>
2019-03-20 23:15:43 -0400

    working version of get command

--- bot.py
+++ bot.py
@@ -15,59 +15,59 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 BOT_ROLE = "bots"
 
-HOUSING_ROLE_IDS = {'501529720932925441', ('alumni', 'alum', 'alumn'),
-                    '444332276483096586', ('sylvan', 'syl'),
-                    '444332646307201034', ('ohill', 'orchard hill', 'o hill'),
-                    '444332880894754818', ('central'), 
-                    '444332735838814210', ('southwest', 'sw', 'swest'),
-                    '444332948322517003', ('northeast', 'ne'),
-                    '444588763427897344', ('north apts', 'north apartments', 'north apartment'), 
-                    '444333125670010890', ('honors college'),
-                    '405025553448566795', ('off-campus', 'off campus', 'offcampus', 'commute', 'commuter'),
-                    '524016335299280908', ('prospective student', 'hs', 'high school')
+HOUSING_ROLE_IDS = {'501529720932925441': ('alumni', 'alum', 'alumn'),
+                    '444332276483096586': ('sylvan', 'syl'),
+                    '444332646307201034': ('ohill', 'orchard hill', 'o hill'),
+                    '444332880894754818': ('central',), 
+                    '444332735838814210': ('southwest', 'sw', 'swest'),
+                    '444332948322517003': ('northeast', 'ne'),
+                    '444588763427897344': ('north apts', 'north apartments', 'north apartment'), 
+                    '444333125670010890': ('honors college',),
+                    '405025553448566795': ('off-campus', 'off campus', 'offcampus', 'commute', 'commuter'),
+                    '524016335299280908': ('prospective student', 'hs', 'high school')
 }
 
-MAJOR_ROLE_IDS = {'442786317273792523', ('electrical engineering', 'ee', 'electrical-engineering'),
-                  '506504010585604132', ('computer engineering', 'ce', 'comp-engineering', 'computer-engineering'),
-                  '504715524152754199', ('biomedical engineering', 'be', 'bio-engineering'),
-                  '506504177242079241', ('environmental engineering', 'environmental-engineering'),
-                  '506211361945288735', ('civil engineering', 'civil-engineering'),
-                  '501524792436981791', ('industrial and mechanical engineering', 'ime', 'industrial engineering', 'me', 'ie', 'mechanical engineering', 'industrial mechanical engineering'),
-                  '439552642415460353', ('ece', 'electrical and computer engineering', 'ec engineering'),
-                  '387619060633829377', ('computer science', 'cs', 'compsci', 'comp-sci'),
-                  '442784019369951252', ('environmental science', 'es'),
-                  '442785279493799966', ('math', 'mathematics'),
-                  '387670593987805184', ('economics', 'econ'),
-                  '405032140040830998', ('informatics', 'info'),
-                  '506223375056633856', ('information technology', 'information tech', 'it'),
-                  '405035782269829121', ('political science', 'polisci', 'poli-sci'),
-                  '442784136457879567', ('biology', 'bio'),
-                  '442784769273626626', ('plant science'), 
-                  '442822241135230978', ('geology', 'geo'), 
-                  '506253630714806272', ('food science'),
-                  '443558312794128407', ('history'),
-                  '447463828398145537', ('physics'),
-                  '536247576496701440', ('bdic', 'bachelors degree with individual concentration'),
-                  '405932216808505355', ('communications', 'communication', 'comm', 'com'),
-                  '490210619270889486', ('nutrition'),
-                  '501525484257935360', ('biochemistry', 'biochem'),
-                  '501608246579167233', ('microbiology', 'microbio'),
-                  '502556060171894785', ('animal science', 'animal'),
-                  '502624207390244864', ('animation'),
-                  '507634130842812441', ('business', 'isenberg'),
-                  '509005908072726533', ('accounting', 'account'),
-                  '517414427390378041', ('linguistics', 'ling'),
-                  '551464859301314572', ('comparative literature', 'comp lit', 'comp-lit'),
-                  '522165967481208833', ('chinese'),
-                  '522166045939597335', ('japanese'),
-                  '524039481486213151', ('psychology', 'psych'),
-                  '543109471136645161', ('public health', 'pub health', 'pub hlth'),
-                  '541744140191268894', ('women, gender, and sexuality studies'),
-                  '524777786972307477', ('education', 'educ'),
-                  '539870761754820628', ('english'),
-                  '387619488880787457', ('cics exploratory', 'cs exploratory', 'cs explo', 'exploratory computer science', 'computer science exploratory', 'exporatory cs', 'exploratory'), 
-                  '506211413564325919', ('engineering undecided', 'engineering-undecided', 'undecided engineering', 'engineering'),
-                  '501908170654875648', ('undecided')
+MAJOR_ROLE_IDS = {'442786317273792523': ('electrical engineering', 'ee', 'electrical-engineering'),
+                  '506504010585604132': ('computer engineering', 'ce', 'comp-engineering', 'computer-engineering'),
+                  '504715524152754199': ('biomedical engineering', 'be', 'bio-engineering'),
+                  '506504177242079241': ('environmental engineering', 'environmental-engineering'),
+                  '506211361945288735': ('civil engineering', 'civil-engineering'),
+                  '501524792436981791': ('industrial and mechanical engineering', 'ime', 'industrial engineering', 'me', 'ie', 'mechanical engineering', 'industrial mechanical engineering'),
+                  '439552642415460353': ('ece', 'electrical and computer engineering', 'ec engineering'),
+                  '387619060633829377': ('computer science', 'cs', 'compsci', 'comp-sci'),
+                  '442784019369951252': ('environmental science', 'es'),
+                  '442785279493799966': ('math', 'mathematics'),
+                  '387670593987805184': ('economics', 'econ'),
+                  '405032140040830998': ('informatics', 'info'),
+                  '506223375056633856': ('information technology', 'information tech', 'it'),
+                  '405035782269829121': ('political science', 'polisci', 'poli-sci'),
+                  '442784136457879567': ('biology', 'bio'),
+                  '442784769273626626': ('plant science',), 
+                  '442822241135230978': ('geology', 'geo'), 
+                  '506253630714806272': ('food science',),
+                  '443558312794128407': ('history',),
+                  '447463828398145537': ('physics',),
+                  '536247576496701440': ('bdic', 'bachelors degree with individual concentration'),
+                  '405932216808505355': ('communications', 'communication', 'comm', 'com'),
+                  '490210619270889486': ('nutrition',),
+                  '501525484257935360': ('biochemistry', 'biochem'),
+                  '501608246579167233': ('microbiology', 'microbio'),
+                  '502556060171894785': ('animal science', 'animal'),
+                  '502624207390244864': ('animation',),
+                  '507634130842812441': ('business', 'isenberg'),
+                  '509005908072726533': ('accounting', 'account'),
+                  '517414427390378041': ('linguistics', 'ling'),
+                  '551464859301314572': ('comparative literature', 'comp lit', 'comp-lit'),
+                  '522165967481208833': ('chinese',),
+                  '522166045939597335': ('japanese',),
+                  '524039481486213151': ('psychology', 'psych'),
+                  '543109471136645161': ('public health', 'pub health', 'pub hlth'),
+                  '541744140191268894': ('women, gender, and sexuality studies'),
+                  '524777786972307477': ('education', 'educ'),
+                  '539870761754820628': ('english',),
+                  '387619488880787457': ('cics exploratory', 'cs exploratory', 'cs explo', 'exploratory computer science', 'computer science exploratory', 'exporatory cs', 'exploratory'), 
+                  '506211413564325919': ('engineering undecided', 'engineering-undecided', 'undecided engineering', 'engineering'),
+                  '501908170654875648': ('undecided',)
 }
                    
 client = Bot(command_prefix=BOT_PREFIX)
@@ -117,8 +117,6 @@ async def on_message(message):
             await client.remove_roles(member, mhom) #removes missing housing or major role
     await client.process_commands(message)
 
-# @client.command(name='getrole')
-
 @client.command(name='8ball',
                 description="Answers from the 8ball",
                 pass_context = True)
@@ -131,9 +129,38 @@ async def eight_ball(context):
     ]
     await client.say(random.choice(possible_responses))
 
+def merge_dict(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
+
 @client.command(name='get', pass_context = True)
-async def get_role(ctx):
-    print("s")
+async def get_role(requested_role):
+    member = requested_role.message.author
+    print(requested_role.message.content[5:])
+    if requested_role.message.server.id == '387465995176116224':
+        available_roles = merge_dict(HOUSING_ROLE_IDS, MAJOR_ROLE_IDS)
+        for role_names in available_roles.values():
+            for option in role_names:
+                if requested_role.message.content[5:] == option: # valid role
+                    # check if member already has the requested role
+                    for member_role in member.roles:
+                        if member_role.name.lower() == role_names[0]:
+                            await client.say("I'm sorry, " + member.mention + ", you already have this role!")
+                            return
+                    # if the member doesnt already have the requested role
+                    for role in requested_role.message.server.roles:
+                        if role.name.lower() == role_names[0]:
+                            role_to_add = role
+                    await client.add_roles(member, role_to_add)
+                    await client.say("Added the " + role_to_add.name + "role to " + member.mention)
+                    return
+        await client.say("I'm sorry, " + member.mention + ", there is no role with that name!\nUse the $getlist command to see the available roles") # invalid role requested  
+    else:
+        await client.say("Roles are not yet supported on this server")
+
+# add command to list gettable roles
+# move checking whether missing a housing or major role to after get is called instead of every message
 
 @client.command(name='mdraw', pass_context = True)
 async def mdraw(ctx):
