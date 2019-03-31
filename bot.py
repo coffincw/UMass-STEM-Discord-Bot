d61076554478c55d61ac5ece982c1de9befff271
Caleb Coffin <coffincw@gmail.com>
2019-03-31 13:37:17 -0400

    regenerated bot token

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ import os
 import time
 
 BOT_PREFIX = "$"
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_TOKEN = os.environ.get('BOT_TOKEN2')
 BOT_ROLE = "bots"
 
 bot_last_command = {} #Key = User ID, Value = Bot's most recent message tied to the command
