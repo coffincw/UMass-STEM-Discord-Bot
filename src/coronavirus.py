463ae6d98474005ba04cc739dc60c4dfe49cff96
Caleb Coffin <coffincw@gmail.com>
2020-03-30 22:32:53 -0400

    Add abbreviation support and display % of pop

--- src/coronavirus.py
+++ src/coronavirus.py
@@ -4,6 +4,59 @@ import os
 
 FINNHUB_CORONA_TOKEN = os.environ.get('FINNHUB_API_TOKEN_5')
 
+us_areas = {'AL': ['Alabama', '4903185'],
+            'AK': ['Alaska', '731545'],
+            'AZ': ['Arizona', '7278717'],
+            'AR': ['Arkansas', '3017804'],
+            'CA': ['California', '39512223'],
+            'CO': ['Colorado', '5758736'],
+            'CT': ['Connecticut', '3565278'],
+            'DE': ['Delaware', '973764'],
+            'FL': ['Florida', '21477737'],
+            'GA': ['Georgia', '10617423'],
+            'HI': ['Hawaii', '1415872'],
+            'ID': ['Idaho', '1787065'],
+            'IL': ['Illinois', '12671821'],
+            'IN': ['Indiana', '6732219'],
+            'IA': ['Iowa', '3155070'],
+            'KS': ['Kansas', '2913314'],
+            'KY': ['Kentucky', '4467673'],
+            'LA': ['Louisiana', '4648794'],
+            'ME': ['Maine', '1344212'],
+            'MD': ['Maryland', '6045680'],
+            'MA': ['Massachusetts', '6892503'],
+            'MI': ['Michigan', '9986857'],
+            'MN': ['Minnesota', '5639632'],
+            'MS': ['Mississippi', '2976149'],
+            'MO': ['Missouri', '6137428'],
+            'MT': ['Montana', '1068778'],
+            'NE': ['Nebraska', '1934408'],
+            'NV': ['Nevada', '3080156'],
+            'NH': ['New Hampshire', '1359711'],
+            'NJ': ['New Jersey', '8882190'],
+            'NM': ['New Mexico', '2096829'],
+            'NY': ['New York', '19453561'],
+            'NC': ['North Carolina', '10488084'],
+            'ND': ['North Dakota', '762062'],
+            'OH': ['Ohio', '11689100'],
+            'OK': ['Oklahoma', '3956971'],
+            'OR': ['Oregon', '4217737'],
+            'PA': ['Pennsylvania', '12801989'],
+            'RI': ['Rhode Island', '1059361'],
+            'SC': ['South Carolina', '5148714'],
+            'SD': ['South Dakota', '884659'],
+            'TN': ['Tennessee', '6829174'],
+            'TX': ['Texas', '28995881'],
+            'UT': ['Utah', '3205958'],
+            'VT': ['Vermont', '623989'],
+            'VA': ['Virginia', '8535519'],
+            'WA': ['Washington', '7614893'],
+            'WV': ['West Virginia', '1792147'],
+            'WI': ['Wisconsin', '5822434'],
+            'WY': ['Wyoming', '578759'],
+            'DC': ['District of Columbia', '705749']
+            }
+
 async def coronavirus(ctx):
     """Generate coronavirus statistics
 
@@ -17,8 +70,15 @@ async def coronavirus(ctx):
         embed = discord.Embed(title='Coronavirus Statistics', color=discord.Color.teal())
         i = 1
         for state in sorted(data, key=lambda state: state['case'], reverse=True): # iterate through the state blocks sorted by case number
+            population = -1
+            comp = [value for key, value in us_areas.items() if str(state['state']).strip() in value]
+            if len(comp) > 0:
+                population = int(comp[0][1])
+                population = round((state['death']/population) * 100, 4)
+            
+            pop_percentage = ' (' + str(population) + '%)' if population != -1 else ''
             fatal_perc = state['death']/state['case'] * 100 # calculate fatality rate
-            cases_output = '{:,d}'.format(state['case']) + '\n' # format integers with commas
+            cases_output = '{:,d}'.format(state['case']) + pop_percentage + '\n' # format integers with commas
             deaths_output = '{:,d}'.format(state['death']) + ' ({:,.2f}%)'.format(fatal_perc)
             embed.add_field(name = str(i) + '. ' + state['state'] + '\n', value = 'Cases: ' + cases_output + 'Deaths: ' + deaths_output, inline=True)
             i += 1
@@ -27,13 +87,25 @@ async def coronavirus(ctx):
         await ctx.send(embed=embed)
 
     else:
-        state = capitalize_all_words(argument)
+        population = -1
+        try:
+            state = us_areas[argument.upper()][0]
+            population = int(us_areas[argument.upper()][1])
+        except:
+            state = capitalize_all_words(argument)
+            comp = [value for key, value in us_areas.items() if state in value]
+            if len(comp) > 0:
+                population = int(comp[0][1])
+        
+        
+
         found = False
         description = ''
         for block in data:
             if str(block['state']).strip() == state:
                 found = True
-                description = 'Cases: ' + '{:,d}'.format(block['case'])
+                pop_percentage = ' ({:,.5f}%)'.format((block['death']/population) * 100) if population != -1 else ''
+                description = 'Cases: ' + '{:,d}'.format(block['case']) + pop_percentage
                 description += '\nDeaths: ' + '{:,d}'.format(block['death'])
                 description += '\nFatality Rate: ' + '{:,.2f}%'.format((block['death']/block['case']) * 100)
                 break
