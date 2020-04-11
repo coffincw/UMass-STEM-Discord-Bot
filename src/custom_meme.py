ea13415b5d98741df3bfc01dd75f39de949546a5
Caleb Coffin <coffincw@gmail.com>
2020-04-10 22:53:02 -0400

    Implement recursive meme commands + make bot type

--- src/custom_meme.py
+++ src/custom_meme.py
@@ -19,19 +19,28 @@ async def draw_universal(ctx, path, command_end_index, origin, name):
         - name: output file name
     """
     channel = ctx.channel
-    url = over.get_image_url(ctx.message, command_end_index)
-    # no input given, try to use previous bot output   
-        
-    if url == 0: # no url, hand should write the inputed text
-        output = over.draw_text(ctx.message.content[command_end_index:], Path(path), origin)
-    else: # url inputed, hand should draw on the image
-        output = over.overlay_image(over.url_to_image(url), Path(path), origin)
-    output.save(name + '.png')
-    try:
-        message = await channel.send(file=discord.File(name + '.png'))
-    except:
-        message = await channel.send(embed=discord.Embed(description="Image too large", color=discord.Color.red()))
-    track_command(ctx.author.id, message) # tracks the most recent command of a user
+    async with channel.typing():
+        url = over.get_image_url(ctx.message, command_end_index) 
+        if url == 0: # no url, hand should write the inputed text 
+            if len(ctx.message.content[command_end_index:].strip()) == 0:
+                # no input given, try to use previous bot output 
+                if bot_last_command[ctx.author.id] is not None:
+                    message = bot_last_command[ctx.author.id]
+                    url = over.get_image_url(message, 0)
+                    output = over.overlay_image(over.url_to_image(url), Path(path), origin)
+                else:
+                    await channel.send(embed=discord.Embed(description="No previous bot command to pull from. Please specify text or an image.", color=discord.Color.red()))
+                    return
+            else:
+                output = over.draw_text(ctx.message.content[command_end_index:], Path(path), origin)
+        else: # url inputed, hand should draw on the image
+            output = over.overlay_image(over.url_to_image(url), Path(path), origin)
+        output.save(name + '.png')
+        try:
+            message = await channel.send(file=discord.File(name + '.png'))
+        except:
+            message = await channel.send(embed=discord.Embed(description="Image too large", color=discord.Color.red()))
+        track_command(ctx.author.id, message) # tracks the most recent command of a user
     os.remove(name + '.png')
 
 async def ify(ctx, scale, path, file_name, *args):
@@ -45,26 +54,28 @@ async def ify(ctx, scale, path, file_name, *args):
         - file_name: output file name
     """
     channel = ctx.channel
-
-    url = over.get_image_url_args(ctx.message, args[0], 1, 0)
-
-    
-    if url == 0: # invalid image
-        await channel.send(embed=discord.Embed(description="Invalid image", color=discord.Color.red()))
-        return
-    else:
+    async with channel.typing():
+        url = over.get_image_url_args(ctx.message, args[0], 1, 0)
+        if url == 0: # invalid image
+            # no input given, try to use previous bot output 
+            if bot_last_command[ctx.author.id] is not None:
+                message = bot_last_command[ctx.author.id]
+                url = over.get_image_url(message, 0)           
+            else:
+                await channel.send(embed=discord.Embed(description="No previous bot command to pull from. Please specify a valid image.", color=discord.Color.red()))
+                return
         output = paste_on_face(Path(path), url, scale)
-    # if there were no faces found then send error
-    if output == 0:
-        await channel.send(embed=discord.Embed(description='No faces found, please input another image', color=discord.Color.red()))
-        return
+        # if there were no faces found then send error
+        if output == 0:
+            await channel.send(embed=discord.Embed(description='No faces found, please input another image', color=discord.Color.red()))
+            return
 
-    output.save(file_name)
-    try:
-        message = await channel.send(file=discord.File(file_name))
-        track_command(ctx.author.id, message)
-    except:
-        await channel.send(embed=discord.Embed(description="Image too large", color=discord.Color.red()))
+        output.save(file_name)
+        try:
+            message = await channel.send(file=discord.File(file_name))
+            track_command(ctx.author.id, message)
+        except:
+            await channel.send(embed=discord.Embed(description="Image too large", color=discord.Color.red()))
     os.remove(file_name)
 
 async def zoomcam(ctx, path, file_name, *args):
@@ -77,24 +88,28 @@ async def zoomcam(ctx, path, file_name, *args):
         - args: arguments of the message
     """
     channel = ctx.channel
-
-    url = over.get_image_url_args(ctx.message, args[0], 1, 0)
-    if url == 0: # invalid image
-        await channel.send(embed=discord.Embed(description="Invalid image", color=discord.Color.red()))
-        return
-    else:
+    async with channel.typing():
+        url = over.get_image_url_args(ctx.message, args[0], 1, 0)
+        if url == 0: # invalid image
+            # no input given, try to use previous bot output 
+            if bot_last_command[ctx.author.id] is not None:
+                message = bot_last_command[ctx.author.id]
+                url = over.get_image_url(message, 0)           
+            else:
+                await channel.send(embed=discord.Embed(description="No previous bot command to pull from. Please specify a valid image.", color=discord.Color.red()))
+                return
         output = over.paste_in_streamer_corner(Path(path), url)
 
-    if output == 0:
-        await channel.send(embed=discord.Embed(description="Encountered issue converting image to RGBA, please try a different image", color=discord.Color.red()))
-        return
+        if output == 0:
+            await channel.send(embed=discord.Embed(description="Encountered issue converting image to RGBA, please try a different image", color=discord.Color.red()))
+            return
 
-    output.save(file_name)
-    try:
-        message = await channel.send(file=discord.File(file_name))
-        track_command(ctx.author.id, message)
-    except:
-        await channel.send(embed=discord.Embed(description="Image too large", color=discord.Color.red()))
+        output.save(file_name)
+        try:
+            message = await channel.send(file=discord.File(file_name))
+            track_command(ctx.author.id, message)
+        except:
+            await channel.send(embed=discord.Embed(description="Image too large", color=discord.Color.red()))
     os.remove(file_name)
 
 
