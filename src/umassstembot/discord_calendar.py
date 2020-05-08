6d337862d2ad4ac0b76aaed835ec4bc1e213a9b1
Caleb Coffin <coffincw@gmail.com>
2020-05-08 13:54:16 -0400

    basic get_calendar working

--- src/umassstembot/discord_calendar.py
+++ src/umassstembot/discord_calendar.py
@@ -1,6 +1,7 @@
 import datetime
 import discord
 import pickle
+import bs4
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -13,6 +14,9 @@ from oauth2client.client import GoogleCredentials, OAuth2WebServerFlow
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+
+WEEKDAYS = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+MONTHS = ("January", "February")
 
 def get_credentials():
     home_dir = os.path.expanduser('~')
@@ -35,33 +39,19 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials 
 
+def convert_time(str_time):
+    date_time = str_time.split('T')
+    mil_time = date_time[1].split('-')[0]
+    time = datetime.datetime.strptime(mil_time, '%H:%M:%S').strftime('%I:%M:%S %p')
+    return date_time[0], time
+
 async def get_events(ctx):
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
     creds = None
     creds = get_credentials()
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-
-    # if os.path.exists('token.pickle'):
-    #     with open('token.pickle', 'rb') as token:
-    #         creds = pickle.load(token)
-    # print(creds)
-    # # If there are no (valid) credentials available, let the user log in.
-    # if not creds or not creds.valid:
-    #     if creds and creds.expired and creds.refresh_token:
-    #         creds.refresh(Request())
-    #     else:
-    #         flow = InstalledAppFlow.from_client_secrets_file(
-    #             'credentials.json', SCOPES)
-    #         creds = flow.run_local_server(port=0)
-    #     # Save the credentials for the next run
-    #     with open('token.pickle', 'wb') as token:
-    #         pickle.dump(creds, token)
-    
-    print(creds)
+   
     service = build('calendar', 'v3', credentials=creds)
 
     # Call the Calendar API
@@ -71,13 +61,32 @@ async def get_events(ctx):
     events_result = service.events().list(calendarId='hca1n2eds4ohvrrg117jkodmk8@group.calendar.google.com', timeMin=now,
                                         maxResults=10, singleEvents=True,
                                         orderBy='startTime').execute()
-    #print(events_result)
     events = events_result.get('items', [])
-
+    #print(events)
+    
     if not events:
-        print('No upcoming events found.')
+        calendar_output_embed = discord.Embed(title=events_result['summary'], description='No upcoming events!', color=discord.Color.red())
+    else:
+        calendar_output_embed = discord.Embed(title=events[0]['organizer'].get('displayName'), color=discord.Color.dark_teal())
     for event in events:
+        
+        #print(event)
         start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+        time = convert_time(start)
+        date = time[0].split('-')
+        date_time_date= datetime.date(int(date[0]), int(date[1]), int(date[2]))
+        month = date_time_date.strftime('%b')
+        day = date_time_date.weekday()
+        day_str = WEEKDAYS[day]
+        title = ''
+        calendar_event_link = '[Calendar Event Link](' + event['htmlLink'] + ')'
+        event_desc = time[1] + ' ' + day_str + ' ' + month + ' ' + date[2] + ' ' + date[0] + '\n' + calendar_event_link
+        if 'description' in event and '<a href=' in event['description'].strip():
+            soup = bs4.BeautifulSoup(event['description'])
+            aTags = soup.find_all("a")
+            urls = [tag['href'] for tag in aTags if 'href' in tag.attrs]
+            te_link = '[Tournament Event Link](' + urls[0] + ')' # tournament link needs to be the first url
+            event_desc += '\n' + te_link
+        calendar_output_embed.add_field(name=event['summary'], value= event_desc, inline=False)
 
-    await ctx.send(embed=discord.Embed(description="yay", color=discord.Color.green()))
+    await ctx.send(embed=calendar_output_embed)
