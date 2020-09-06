858be9ba010d0389cfa1b08685cc51b9cc8d14ea
Caleb Coffin <coffincw@gmail.com>
2020-09-06 00:33:44 -0400

    Add comment to del_convo

--- src/umassstembot/bot_helper.py
+++ src/umassstembot/bot_helper.py
@@ -3,5 +3,13 @@ import asyncio
 
 
 async def del_convo(user_msg, bot_msg, delay):
+    """
+    Deletes both the user and bot message after the specified delay in seconds
+
+    Parameters:
+    - user_msg: message object for the user's message
+    - bot_msg: message object for the bot's message
+    - delay: int representing the number of seconds to delay before deleting
+    """
     await bot_msg.delete(delay=delay)
     await user_msg.delete(delay=delay)
